ea1cd99b5f0d8489df92583cf684cec152fe35fe
Sanzio Monti <github@sanziomonti.xyz>
2022-11-23 11:23:07 +0100

    remove unused files / components

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,20 +50,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/content/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `projects`,
-        path: `${__dirname}/content/projects`,
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
